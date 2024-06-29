9eee90fee303ec6afe1ab3ff2190d9826ae74b2f
mohammed imran <118888341+IMRAN-RS@users.noreply.github.com>
2024-06-29 14:42:01 +0530

    Add files via upload

---
+++ User_Authentication/src/main/java/com/imran/authapi/controllers/AuthenticationController.java
@@ -0,0 +1,43 @@
+package com.imran.authapi.controllers;
+
+import com.imran.authapi.dtos.LoginUserDto;
+import com.imran.authapi.dtos.RegisterUserDto;
+import com.imran.authapi.entities.User;
+import com.imran.authapi.responses.LoginResponse;
+import com.imran.authapi.services.AuthenticationService;
+import com.imran.authapi.services.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signup(registerUserDto);
+
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+
+        LoginResponse loginResponse = new LoginResponse().setToken(jwtToken).setExpiresIn(jwtService.getExpirationTime());
+
+        return ResponseEntity.ok(loginResponse);
+    }
+}
